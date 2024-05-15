486b5530a2a3d95c552360c7553811ac5732da97
marcos <marcosmellebovsky@gmail.com>
2024-05-15 19:50:08 -0300

    casi  terminado

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-const BIENVENIDA = alert("Bienvenido al cajero automático");
-  let saldo = 100;
+const BIENVENIDA = alert("Bienvenido al cajero automático 👋👋");
+let saldo;
+saldo = parseInt(prompt("Ingrese su saldo!!"));
+
+while (saldo <= 0){
+saldo = parseInt(prompt("Error! Vuelva a ingresar su saldo!"));
+}
+let resultado;
   do {
     
     opcion = parseInt(prompt(`Bienvenido al cajero automático
-  
   ¿Qué desea hacer?
   
   1. Consultar saldo
   2. Retirar efectivo
   3. Depositar efectivo
   4. Salir
-  
+
   Ingrese su opción: `));
   
     switch (opcion) {
@@ -18,13 +23,13 @@ const BIENVENIDA = alert("Bienvenido al cajero automático");
         consultarSaldo(saldo);
         break;
       case 2:
-        rtirarEfectivo(saldo);
+        rtirarEfectivo(saldo,resultado);
         break;
       case 3:
-        
+        depositarEfectivo(saldo);
         break;
       case 4:
-        
+          alert("Gracias por usar el cajero automático.❤️❤️ ");
         break;
       default:
         alert("Opción inválida.");
@@ -32,25 +37,44 @@ const BIENVENIDA = alert("Bienvenido al cajero automático");
   } while (opcion !== 4);
 
 function consultarSaldo(saldo) {
-    alert("Su saldo actual es de: " + "$"+ saldo);
+  alert("Su saldo actual es de: " + "$"+ saldo);
 }
-function rtirarEfectivo(saldo) {
-    let saldoRetirado =  prompt("Cuanto desea retirar? ");
-    let resulatdo;
-    if (saldoRetirado < saldo) {
-         alert("No puedes retirar esa cantidad de dinero, ya que tienes " + "$" + saldo + " en tu cuenta");
+function rtirarEfectivo(saldo,resultado) {  
+    let saldoRetirado
+     saldoRetirado = parseInt(prompt("¿Cuánto desea retirar?"));
+    while (saldoRetirado <= 0){
+      saldoRetirado = parseInt(prompt("Error! Vuelva a ingresar la cantidad de saldo a retirar!"));
     }
-    do {
-       
-    } 
-    while (saldoRetirado < saldo);
-   
-    resulatdo = saldo - saldoRetirado;
-    alert("El saldo que te quedo en cuenta es de: " + "$" + resulatdo);
-    
+    let estaSeguro = prompt("Está seguro de que desea retirar: " + "$" + saldoRetirado + "? " + "ingrese: si, si esta seguro ingrese no, si no esta seguro");
 
+    if (estaSeguro == "si") {
+    if (saldoRetirado <= saldo) {
+        resultado = saldo - saldoRetirado;
+        alert("El saldo que te quedó en cuenta es de: $" + resultado);
+    } 
+    else {
+        do {
+            saldoRetirado = parseInt(prompt("Error! Vuelve a ingresar una cantidad menor o igual que $" + saldo));
+        } while (saldoRetirado > saldo);
+
+        resultado = saldo - saldoRetirado;
+        alert("El saldo que te quedó en cuenta es de: $" + resultado);
+    } 
+  }
   
-     
-    
-    
+}
+
+function depositarEfectivo(saldo) {
+  let depositoMaximo = 20000;
+  let saldoDepositado = parseInt(prompt(`¿Cuánto desea depositar en su cuenta? Máximo a depositar: $${depositoMaximo}`));
+
+  if (saldoDepositado <= 0) {
+      alert("Error! Ingrese un monto válido.");
+  } else if (saldoDepositado > depositoMaximo) {
+      alert("No se puede depositar esa cantidad, máximo hasta: $" + depositoMaximo);
+  } else {
+      saldo = saldo + saldoDepositado;
+      alert("El saldo que te quedó en cuenta es de: $" + saldo);
+  }
+  return saldo;
 }
